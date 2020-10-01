ebd4675a303bce7fc25874c7a3a56eaf6ab70259
clever laura <clever@ronald.com>
2020-10-01 10:54:14 -0500

    commit 3

--- movies/common/utils.go
+++ movies/common/utils.go
@@ -7,36 +7,12 @@ import (
 	"os"
 	"time"
 
-	"gopkg.in/mgo.v2"
+	
 )
 
-type (
-	appError struct {
-		Error      string `json:"error"`
-		Message    string `json:"message"`
-		HttpStatus int    `json:"status"`
-	}
-	errorResource struct {
-		Data appError `json:"data"`
-	}
-	configuration struct {
-		Server, MongoDBHost, DBUser, DBPwd, Database string
-	}
-)
 
-func DisplayAppError(w http.ResponseWriter, handlerError error, message string, code int) {
-	errObj := appError{
-		Error:      handlerError.Error(),
-		Message:    message,
-		HttpStatus: code,
-	}
-	log.Printf("AppError]: %s\n", handlerError)
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	w.WriteHeader(code)
-	if j, err := json.Marshal(errorResource{Data: errObj}); err == nil {
-		w.Write(j)
-	}
-}
+
+
 
 // AppConfig holds the configuration values from config.json file
 var AppConfig configuration
@@ -93,35 +69,3 @@ func loadConfigFromEnvironment(appConfig *configuration)  {
 }
 
 // Session holds the mongodb session for database access
-var session *mgo.Session
-
-// GetSession gets database session
-func GetSession() *mgo.Session {
-	if session == nil {
-		var err error
-		session, err = mgo.DialWithInfo(&mgo.DialInfo{
-			Addrs:    []string{AppConfig.MongoDBHost},
-			Username: AppConfig.DBUser,
-			Password: AppConfig.DBPwd,
-			Timeout:  60 * time.Second,
-		})
-		if err != nil {
-			log.Fatalf("[GetSession]: %s\n", err)
-		}
-	}
-	return session
-}
-
-// Create database session
-func createDbSession() {
-	var err error
-	session, err = mgo.DialWithInfo(&mgo.DialInfo{
-		Addrs:    []string{AppConfig.MongoDBHost},
-		Username: AppConfig.DBUser,
-		Password: AppConfig.DBPwd,
-		Timeout:  60 * time.Second,
-	})
-	if err != nil {
-		log.Fatalf("[createDbSession]: %s\n", err)
-	}
-}
